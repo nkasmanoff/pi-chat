fbd94b24cc1ae73010b5f1519131d9f00b5de36c
Noah Kasmanoff <nkasmanoff@gmail.com>
2024-04-22 13:22:57 -0400

    add image chat

--- image_app.py
+++ image_app.py
@@ -1,13 +1,33 @@
-import ollama
-import gradio as gr
-import os
+import streamlit as st
 import random
+import os
+import ollama
 
-def answer_question(img, prompt):
-    # save image to disk. Add this random str on top to avoid the very low chance of mulitple users using this at the same time.
+
+
+st.title("Tiny Chat with Images")
+
+st.write("This is a chatbot that can help you with your questions. Ask me anything.")
+st.write("Sadly this is pretty slow program so you may need to wait a few minutes.")
+
+# file uploader
+uploaded_file = st.file_uploader("Choose an image...", 
+                                 # type is jpg or png
+                                    type=["jpg", "png"])    
+
+
+# text input
+prompt = st.text_input("Input Question", "")
+submit = st.button("Submit")
+
+
+if uploaded_file and submit:
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+
     random_str = str(random.randint(0, 100000))
-    img.save(f"image_{random_str}.jpg")
-
+    with open(f"image_{random_str}.jpg", "wb") as f:
+        f.write(uploaded_file.read())
+    
     res = ollama.chat(
         model="bakllava:7b-v1-q2_K",
         messages=[
@@ -20,24 +40,7 @@ def answer_question(img, prompt):
     )
     # delete image
     os.remove(f"image_{random_str}.jpg")  
-    return res['message']['content']
-
-
-
-with gr.Blocks() as demo:
-    gr.Markdown(
-        """
-        # Tiny chat (with images)
-        ### Upload an image, wait about 5 minutes, and get an answer to your question!
-        """
-    )
-    with gr.Row():
-        prompt = gr.Textbox(label="Input Prompt", placeholder="Type here...", scale=4)
-        submit = gr.Button("Submit")
-    with gr.Row():
-        img = gr.Image(type="pil", label="Upload an Image")
-        output = gr.TextArea(label="Response")
-    submit.click(answer_question, [img, prompt], output)
-    prompt.submit(answer_question, [img, prompt], output)
-
-demo.queue().launch(debug=True)
+    st.write(res['message']['content'])
+else:
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+    
